4e191f8084125a0b3ef66154a0baf2d3154104fb
shams-mehdi <shamsmehdi222@gmail.com>
2025-07-30 03:15:22 -0400

    t21

--- MDTerp/__init__.py
+++ MDTerp/__init__.py
@@ -1,15 +1,3 @@
-[project]
 __version__ = "0.1.4"
 __author__ = """shams mehdi"""
 __email__ = "shamsmehdi222@gmail.com"
-requires-python = ">=3.7"
-dependencies = [
-    "numpy",
-    "sklearn",
-    "scipy",
-    "matplotlib"
-]
-
-[build-system]
-requires = ["setuptools", "wheel"]
-build-backend = "setuptools.build_meta"
